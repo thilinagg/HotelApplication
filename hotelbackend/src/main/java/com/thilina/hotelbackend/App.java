c44f8b61e1c77655d5ba91bc1a756635c0bd2023
thilina <thilina.gg@gmail.com>
2018-05-06 13:23:06 +0530

    initial commit

---
+++ hotelbackend/src/main/java/com/thilina/hotelbackend/App.java
@@ -0,0 +1,13 @@
+package com.thilina.hotelbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
